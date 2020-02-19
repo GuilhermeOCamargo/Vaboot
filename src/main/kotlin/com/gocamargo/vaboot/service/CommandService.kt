62e3288737360c66393738a97102749848b2cdc5
Luis Higa <luis.higa@amedigital.com>
2020-02-19 17:28:57 -0300

    chore: change class/interface for function

--- src/main/kotlin/com/gocamargo/vaboot/service/CommandService.kt
+++
@@ -1,7 +0,0 @@
-package com.gocamargo.vaboot.service
-
-import com.gocamargo.vaboot.util.ResponseMessage
-
-interface CommandService {
-    fun handleCommand(requestMessage: String): ResponseMessage
-}
